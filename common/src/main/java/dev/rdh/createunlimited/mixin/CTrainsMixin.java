368c3b1b8dc83a20fb2919c70049ac88fce91e1a
rhysdh540 <rhys_dehaan@ryecountryday.org>
2023-07-24 06:57:28 +0700

    make CTrainsMixin use @ModifyConstant

--- common/src/main/java/dev/rdh/createunlimited/mixin/CTrainsMixin.java
+++ common/src/main/java/dev/rdh/createunlimited/mixin/CTrainsMixin.java
@@ -6,12 +6,12 @@ import com.simibubi.create.infrastructure.config.CTrains;
 import dev.rdh.createunlimited.CreateUnlimited;
 
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
 
-@Mixin(CTrains.class)
+@Mixin(value = CTrains.class, remap = false)
 public class CTrainsMixin {
-	@ModifyArg(method = "<init>()V", at = @At(value = "INVOKE", target = "Lcom/simibubi/create/infrastructure/config/CTrains;i(IIILjava/lang/String;[Ljava/lang/String;)Lcom/simibubi/create/foundation/config/ConfigBase$ConfigInt;", remap = false), index = 2, remap = false)
+	@ModifyConstant(method = "<init>", constant = @Constant(intValue = 128, ordinal = 0))
 	private int modifyMaxTrackPlacementLength(int par1) {
 		CreateUnlimited.LOGGER.info("CTrains config override loaded (probably)");
 		return Integer.MAX_VALUE;
