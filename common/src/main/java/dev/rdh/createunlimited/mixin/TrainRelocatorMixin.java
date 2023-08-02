a6f6d06db24aae21b8e29eba2a11f03899dbfe3a
rhysdh540 <rhys_dehaan@ryecountryday.org>
2023-08-02 12:18:41 +0800

    add mixins

---
+++ common/src/main/java/dev/rdh/createunlimited/mixin/TrainRelocatorMixin.java
@@ -0,0 +1,25 @@
+package dev.rdh.createunlimited.mixin;
+
+
+import com.simibubi.create.content.trains.entity.TrainRelocator;
+
+import dev.rdh.createunlimited.config.CUConfig;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
+
+/**
+ * Mixin to modify the maximum distance a train can be relocated with a wrench.
+ */
+@Mixin(TrainRelocator.class)
+public class TrainRelocatorMixin {
+	@ModifyConstant(method = "onClicked", constant = @Constant(doubleValue = 24), remap = false)
+	private static double modifyMaxTrainRelocatingDistance(double original) {
+		return CUConfig.maxTrainRelocationDistance.get();
+	}
+	@ModifyConstant(method = "clientTick", constant = @Constant(doubleValue = 24), remap = false)
+	private static double modifyMaxTrainRelocatingDistanceClient(double original) {
+		return CUConfig.maxTrainRelocationDistance.get();
+	}
+}
