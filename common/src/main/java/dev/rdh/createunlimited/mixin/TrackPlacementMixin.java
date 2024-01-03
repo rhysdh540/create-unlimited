13db61f53a3494d847db55f3d7ee99febdae2198
Rhys <98863820+rhysdh540@users.noreply.github.com>
2024-01-03 14:18:02 -0500

    pls work :(

--- common/src/main/java/dev/rdh/createunlimited/mixin/TrackPlacementMixin.java
+++ common/src/main/java/dev/rdh/createunlimited/mixin/TrackPlacementMixin.java
@@ -28,7 +28,6 @@ import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.Vec3;
 
-import org.joml.Math;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
