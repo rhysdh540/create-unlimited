3d146d370f102d164fb86355dba3cdbddab8404c
Rhys <98863820+rhysdh540@users.noreply.github.com>
2024-02-10 18:08:00 -0500

    fix? some issues with mod being installed on the client side only

---
+++ common/src/main/java/dev/rdh/createunlimited/mixin/ClientHandshakePacketListenerImplMixin.java
@@ -0,0 +1,41 @@
+package dev.rdh.createunlimited.mixin;
+
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import dev.rdh.createunlimited.CreateUnlimited;
+import dev.rdh.createunlimited.config.CUConfigs;
+
+import net.minecraft.client.multiplayer.ClientHandshakePacketListenerImpl;
+import net.minecraft.client.multiplayer.ServerData;
+import net.minecraft.network.chat.Component;
+
+/**
+ * ensures on server join that the server has unlimited installed (things break otherwise)
+ */
+@Mixin(ClientHandshakePacketListenerImpl.class)
+public class ClientHandshakePacketListenerImplMixin {
+	@Shadow
+	@Final
+	@Mutable
+	private ServerData serverData;
+
+	@Inject(method = "authenticateServer", at = @At("HEAD"), cancellable = true)
+	private void onAuthenticateServer(String serverHash, CallbackInfoReturnable<Component> cir) {
+		CreateUnlimited.LOGGER.info("Checking if Create Unlimited is installed on the server...");
+		try {
+			// this will fail if unlimited is not installed on the server
+			CUConfigs.server().allowAllCopycatBlocks.get();
+		} catch (IllegalStateException e) {
+			Component c = Component.literal("Create Unlimited is not installed on the server!" +
+				"\nPlease either install it or remove it from your client.");
+			serverData = null;
+			cir.setReturnValue(c);
+		}
+	}
+}
