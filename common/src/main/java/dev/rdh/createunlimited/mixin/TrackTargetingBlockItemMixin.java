5d88a947d15a5ac7ae80d034c8b6ff6c46eb339a
Rhys <98863820+rhysdh540@users.noreply.github.com>
2024-02-12 13:39:45 -0500

    add track-targeting block placing distance configuring resolves #20

---
+++ common/src/main/java/dev/rdh/createunlimited/mixin/TrackTargetingBlockItemMixin.java
@@ -0,0 +1,22 @@
+package dev.rdh.createunlimited.mixin;
+
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+import dev.rdh.createunlimited.Util;
+import dev.rdh.createunlimited.config.CUConfigs;
+
+import com.simibubi.create.content.trains.track.TrackTargetingBlockItem;
+
+@Mixin(TrackTargetingBlockItem.class)
+public class TrackTargetingBlockItemMixin {
+	@ModifyExpressionValue(method = "useOn", at = {
+		@At(value = "CONSTANT", args = "doubleValue=80.0"),
+		@At(value = "CONSTANT", args = "doubleValue=16.0"),
+	})
+	private double modifyMaxDistance(double original) {
+		return Util.orElse(CUConfigs.server.maxTrackBlockPlacingDistance, original);
+	}
+}
