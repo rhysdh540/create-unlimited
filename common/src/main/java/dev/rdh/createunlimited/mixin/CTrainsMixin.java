da9a62a967a662873e328863032007e94537fd5b
Rhys <98863820+rhysdh540@users.noreply.github.com>
2024-02-28 12:25:45 -0500

    only apply to 1 thing

--- common/src/main/java/dev/rdh/createunlimited/mixin/CTrainsMixin.java
+++ common/src/main/java/dev/rdh/createunlimited/mixin/CTrainsMixin.java
@@ -11,7 +11,7 @@ import org.spongepowered.asm.mixin.injection.At;
 
 @Mixin(value = CTrains.class, remap = false)
 public abstract class CTrainsMixin {
-	@ModifyExpressionValue(method = "<init>", at = @At(value = "CONSTANT", args = "intValue=128"))
+	@ModifyExpressionValue(method = "<init>", at = @At(value = "CONSTANT", args = "intValue=128", ordinal = 0))
 	private int modifyMaxTrackPlacementLength(int original) {
 		CreateUnlimited.LOGGER.info("CTrains config override loaded (probably)");
 		return Integer.MAX_VALUE;
