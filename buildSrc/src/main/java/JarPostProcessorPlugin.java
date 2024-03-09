1e02a6f5130bacdbadc6fced58db32a45c9d7b64
Rhys <98863820+rhysdh540@users.noreply.github.com>
2024-03-09 20:13:36 +0700

    not again workflow :(

--- buildSrc/src/main/java/JarPostProcessorPlugin.java
+++ buildSrc/src/main/java/JarPostProcessorPlugin.java
@@ -146,7 +146,7 @@ public class JarPostProcessorPlugin implements Plugin<Project> {
 			if(!(node.instructions.get(i + 3) instanceof InsnNode)) continue;
 			AbstractInsnNode insn = node.instructions.get(i + 2);
 			if(insn instanceof MethodInsnNode methodInsn) {
-				//target
+				//target static { IBootstrap.dasBoot(); }
 				if(methodInsn.name.equals("dasBoot") && methodInsn.owner.equals("manifold/rt/api/IBootstrap")
 				&& methodInsn.desc.equals("()Z") && methodInsn.getOpcode() == Opcodes.INVOKESTATIC) {
 					return i;
